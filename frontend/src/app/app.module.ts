3d1e251d3e9d54e74912dd3371b828afd797bc0e
Shyam S <s007shyam2012@gmail.com>
2023-02-14 19:32:16 +0530

    day 1 updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HotelListComponent } from './hotel-list/hotel-list.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    HotelListComponent
+    HotelListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
